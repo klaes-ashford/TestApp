41bef3d5bd8857f33b72abace6c5b17d16a9e216
Aswin K V <aisling.linux@gmail.com>
2020-09-01 13:49:29 +0530

    Initial Commit

---
+++ TestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProject
+//
+//  Created by Aswin Koramanghat on 01/09/20.
+//  Copyright © 2020 Aswin Koramanghat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
